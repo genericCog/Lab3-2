466c499737a236be330f89da734966dd858fcdbf
AdamActual <adamactual.io@gmail.com>
2015-07-22 15:58:18 -0700

    Initial Commit 2015-07-22-1555-AdamActual. Prime number identifier. NEEDS to use Math.Sqrt() function.

---
+++ Lab3-2_AdamCherochak/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Lab3_2_AdamCherochak
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Title = new System.Windows.Forms.Label();
+            this.txtBox_UserInput = new System.Windows.Forms.TextBox();
+            this.lbl_DisplayResult = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.picBox_PrimeOrNot = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox_PrimeOrNot)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_Title
+            // 
+            this.lbl_Title.AutoSize = true;
+            this.lbl_Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Title.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lbl_Title.Location = new System.Drawing.Point(12, 9);
+            this.lbl_Title.Name = "lbl_Title";
+            this.lbl_Title.Size = new System.Drawing.Size(192, 24);
+            this.lbl_Title.TabIndex = 0;
+            this.lbl_Title.Text = "Is It A Prime Number?";
+            // 
+            // txtBox_UserInput
+            // 
+            this.txtBox_UserInput.Location = new System.Drawing.Point(16, 61);
+            this.txtBox_UserInput.Name = "txtBox_UserInput";
+            this.txtBox_UserInput.Size = new System.Drawing.Size(157, 20);
+            this.txtBox_UserInput.TabIndex = 0;
+            this.txtBox_UserInput.Text = "Enter a Number";
+            // 
+            // lbl_DisplayResult
+            // 
+            this.lbl_DisplayResult.AutoSize = true;
+            this.lbl_DisplayResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_DisplayResult.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lbl_DisplayResult.Location = new System.Drawing.Point(55, 99);
+            this.lbl_DisplayResult.Name = "lbl_DisplayResult";
+            this.lbl_DisplayResult.Size = new System.Drawing.Size(138, 20);
+            this.lbl_DisplayResult.TabIndex = 2;
+            this.lbl_DisplayResult.Text = "Prime? Let\'s see...";
+            this.lbl_DisplayResult.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 166);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(118, 166);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Test If Prime";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(221, 166);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // picBox_PrimeOrNot
+            // 
+            this.picBox_PrimeOrNot.Location = new System.Drawing.Point(16, 95);
+            this.picBox_PrimeOrNot.Name = "picBox_PrimeOrNot";
+            this.picBox_PrimeOrNot.Size = new System.Drawing.Size(33, 28);
+            this.picBox_PrimeOrNot.TabIndex = 6;
+            this.picBox_PrimeOrNot.TabStop = false;
+            this.picBox_PrimeOrNot.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 201);
+            this.Controls.Add(this.picBox_PrimeOrNot);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbl_DisplayResult);
+            this.Controls.Add(this.txtBox_UserInput);
+            this.Controls.Add(this.lbl_Title);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picBox_PrimeOrNot)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Title;
+        private System.Windows.Forms.TextBox txtBox_UserInput;
+        private System.Windows.Forms.Label lbl_DisplayResult;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox picBox_PrimeOrNot;
+    }
+}
+
---
+++ Lab3-2_AdamCherochak/Form1.cs
@@ -0,0 +1,95 @@
+﻿/*
+ *  201507201601
+ *  Adam Cherochak
+ *  Assignment 3.2
+ *  A program (C# Windows Forms Application) that will 
+ *      1. accept an integer as input and 
+ *      2. determine if it is a prime number (i.e., divisible only by itself and 1).  
+ *      3. Use the modulo operator (%) which determines the remainder of division of two integers.
+ *  Reference Material: http://stackoverflow.com/questions/15743192/check-if-number-is-prime-number 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3_2_AdamCherochak
+{
+    public partial class Form1 : Form
+    {
+        //global variables
+        bool primeResult = true; // assign flag to true
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {            
+            int primeNumber = Convert.ToInt32(txtBox_UserInput.Text); //assign user input
+            TestIfPrime(primeNumber, primeResult); // test user input
+        }//END Calculate button
+
+        private void TestIfPrime(int primeNumber, bool primeResult){
+            if (primeNumber == 0 || primeNumber == 1) // if 0 or 1, number is not prime
+            {
+                primeResult = false;
+                DisplayConfiguration(primeNumber, primeResult);
+            }
+            else
+            {
+                for (int i = 2; i <= (primeNumber / 2); i++) // how to use Math.Sqrt(primeNumber) here??
+                {
+                    if (primeNumber % i == 0)
+                    {
+                        primeResult = false;
+                        DisplayConfiguration(primeNumber, primeResult);
+                        return;
+                    }
+                }
+                primeResult = true; // the number is prime
+                DisplayConfiguration(primeNumber, primeResult);
+            }            
+        }//END TestIfPrime method
+
+        private void DisplayConfiguration(int primeNumber, bool primeResult)
+        {
+            if (primeResult == true)
+            {
+                lbl_DisplayResult.Visible = true;
+                lbl_DisplayResult.Text = "The number, " + primeNumber + ", IS Prime.";
+                picBox_PrimeOrNot.BackColor = Color.Green;
+                picBox_PrimeOrNot.Visible = true;
+            }
+            else
+            {
+                lbl_DisplayResult.Visible = true;
+                lbl_DisplayResult.Text = "The number, " + primeNumber + ", is NOT Prime.";
+                picBox_PrimeOrNot.BackColor = Color.Red;
+                picBox_PrimeOrNot.Visible = true;
+            }
+        }//END DisplayConfigurations
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtBox_UserInput.Clear(); //reset contents of textbox 1
+            picBox_PrimeOrNot.Visible = false;
+            txtBox_UserInput.Text = "Enter a number";
+            lbl_DisplayResult.Text = "Waiting for input...";
+            txtBox_UserInput.Focus(); //rest focus to textbox 1
+        }//END Clear button
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close(); //exit the program
+        }//END Exit button
+    }//END Class
+}//END Namespace
+
